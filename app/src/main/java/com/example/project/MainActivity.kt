374caf2831091c6f4a637eaf0d2fdeef322d615f
daovincent <vincent-dao@outlook.com>
2022-05-11 15:51:13 +0200

    first commit

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -3,13 +3,15 @@ package com.example.project
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.project.ui.theme.ProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +20,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             ProjectTheme {
                 // A surface container using the 'background' color from the theme
+                MainView()
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MainView()
+                    Row() {
+                        Greeting("Android")
+                    }
                 }
             }
         }
@@ -30,9 +36,42 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun MainView(){
+    Row() {
+
+        Button(
+            onClick = { /* ... */ },
+            // Uses ButtonDefaults.ContentPadding by default
+            contentPadding = PaddingValues(
+                start = 20.dp,
+                top = 12.dp,
+                end = 20.dp,
+                bottom = 12.dp,
+
+            )
+        ) {
+            // Inner content including an icon and a text label
+            Icon(
+                Icons.Filled.Favorite,
+                contentDescription = "Favorite",
+                modifier = Modifier
+                    .size(ButtonDefaults.IconSize)
+                    .height(50.dp),
+            )
+            Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+            Text("Add Tr")
+        }
+    }
+
+
+
+}
+@Composable
 fun Greeting(name: String) {
     var tr=Transaction("test",15,Etiquette.Tag.Food)
+
     Text(text = "$tr")
+
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/example/project/NoteVersion.kt
+++
@@ -1,7 +0,0 @@
-package com.example.project
-
-import java.time.LocalDateTime.now
-
-data class NoteVersion (val montant : Int){
-    val date = now()
-}
