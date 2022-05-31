d50a6c557c6e9e37a9e668cd0f61f40d7cc0c72f
Robin <robin.rieutord@gmail.com>
2022-05-31 19:49:36 +0200

    change transaction

--- app/src/main/java/com/example/project/Transaction.kt
+++ app/src/main/java/com/example/project/Transaction.kt
@@ -4,59 +4,16 @@ import java.time.LocalDateTime.now
 import java.util.*
 import kotlin.collections.ArrayList
 
-class Transaction {
-    private var text: String = ""
-    private var list: ArrayList<Etiquette.Tag>? = null
-    private var montant = 0
+data class Transaction (val carnet : Int, var text: String, var montant: Int, var tags: ArrayList<Etiquette.Tag>){
     private lateinit var date: LocalDateTime
 
+    constructor(old: Transaction) : this(old.carnet,old.text,old.montant,old.tags)
 
-    constructor(text: String, montant: Int, tags: ArrayList<Etiquette.Tag>) {
-        if (montant < 0) {
-            println("Montant peut pas etre negatif")
-            return
-        }
-        Objects.requireNonNull(tags)
-        list=tags
-        date = now()
-        this.text = text
-    }
-
-    constructor(old: Transaction) {
-        montant = old.montant
-        list = old.list
-        date = now()
-        text = old.text
-    }
-
-    fun editMontant(newm: Int) {
-        montant = newm
-    }
-
-    fun editText(newt: String) {
-        text = newt
-    }
-
-    fun removeTag(t: Etiquette.Tag) {
-        list!!.remove(t)
-    }
-
-    fun addTag(t: Etiquette.Tag) {
-        list!!.add(t)
-    }
-    fun tags() : ArrayList<Etiquette.Tag>{
-        return tags()
-    }
-    fun montant(): Int {
-        return montant
-    }
-    fun text() : String{
-        return text
+    fun date() : LocalDateTime{
+        return date
     }
     override fun toString(): String {
-        return "$text, $montant €, for : $list the $date)"
+        return "from $carnet -> $text, $montant €, for : $tags the $date)"
     }
-
-
 }
 
