d64a588400e0706e3800c249cd062fd6b6e267c5
Vincent <vincent-dao@outlook.com>
2022-05-31 22:11:57 +0200

    Rework TR for Room DB + Add DAO + RoomDatabase file + Repository file

---
+++ app/src/main/java/com/example/project/database/TransactionViewModel.kt
@@ -0,0 +1,28 @@
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.example.project.Transaction
+import com.example.project.database.TransactionRepository
+import kotlinx.coroutines.launch
+
+class TransactionViewModel(private val repository: TransactionRepository) : ViewModel() {
+
+    /**
+     * Launching a new coroutine to insert the data in a non-blocking way
+     */
+    fun insert(transaction: Transaction) = viewModelScope.launch {
+        repository.insert(transaction)
+    }
+}
+
+class WordViewModelFactory(private val repository: TransactionRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(TransactionViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return TransactionViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
